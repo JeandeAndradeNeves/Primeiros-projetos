a17206679b4e69584636aef3bf3ff76105b5f524
Jean  Andrade <99501079+JeandeAndradeNeves@users.noreply.github.com>
2022-08-25 17:11:49 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function verificarEntrada () {
-   nomeConvidados = document.getElementById("nome").value;
-   convidadosJean = ["Ana","Lyliane","Natalia","Geova","Itamar","Yago","Francisca","Geraldo","Joice","Jamile","Hellen"];
-   if (convidadosJean.includes(nomeConvidados)){
-     document.getElementById("permissao").innerHTML = "Você pode entrar!"
-   }else{
-    document.getElementById("permissao").innerHTML = "Você não pode entrar!"
-   }
-};
