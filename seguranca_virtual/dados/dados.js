926ee201e371846f4838feababab7761671c80dd
JeandeAndradeNeves <jeandeneves@outlook.com>
2022-09-06 15:50:20 -0300

    aprendendo-usar-api

---
+++ seguranca_virtual/dados/dados.js
@@ -0,0 +1,5 @@
+
+
+let convidadosJean = ["Ana","Lyliane","Natalia","Geova","Itamar","Yago","Francisca","Geraldo","Joice","Jamile","Hellen"];
+
+export default convidadosJean;
--- seguranca_virtual/index.js
+++ seguranca_virtual/index.js
@@ -1,9 +1,13 @@
-function verificarEntrada () {
+import convidadosJean from './dados/dados.js';
+
+
+function verificarEntrada (convidadosJean) {
+
    nomeConvidados = document.getElementById("nome").value;
-   convidadosJean = ["Ana","Lyliane","Natalia","Geova","Itamar","Yago","Francisca","Geraldo","Joice","Jamile","Hellen"];
    if (convidadosJean.includes(nomeConvidados)){
      document.getElementById("permissao").innerHTML = "Você pode entrar!"
    }else{
     document.getElementById("permissao").innerHTML = "Você não pode entrar!"
    }
+
 };
